12d39341083b5c92989ac35381da3ddd1190cf09
FansaOrz <a819096127@gmail.com>
2019-11-05 00:43:40 +0100

    new version

---
+++ ch07/build/CMakeFiles/camera_with_orb.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fansa/Src/SLAM_Learning/ch07/camera_with_orb.cpp" "/home/fansa/Src/SLAM_Learning/ch07/build/CMakeFiles/camera_with_orb.dir/camera_with_orb.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ch07/build/CMakeFiles/camera_with_orb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camera_with_orb.dir/camera_with_orb.cpp.o"
+  "camera_with_orb.pdb"
+  "camera_with_orb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camera_with_orb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
